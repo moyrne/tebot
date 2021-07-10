09f2f2edf8362250c2814c965c463f017da51385
moyrn <Chengpin430124>
2021-07-10 16:25:59 +0800

    签到

--- internal/analyze/signin.go
+++ internal/analyze/signin.go
@@ -6,6 +6,7 @@ import (
 	"github.com/moyrne/tebot/internal/database"
 	"github.com/moyrne/tebot/internal/models"
 	"github.com/moyrne/weather"
+	"github.com/pkg/errors"
 )
 
 var weComCn weather.Weather = weather.WeComCn{}
@@ -14,6 +15,10 @@ func SignIn(ctx context.Context, params Params) (string, error) {
 	// 记录签到信息
 	var area string
 	if err := database.NewTransaction(ctx, func(ctx context.Context, tx *sqlx.Tx) error {
+		err := (&models.QSignIn{QUID: params.QUID}).Insert(ctx, tx)
+		if err != nil {
+			return err
+		}
 		user, err := models.GetQUserByQUID(ctx, tx, params.QUID)
 		if err != nil {
 			return err
@@ -21,17 +26,22 @@ func SignIn(ctx context.Context, params Params) (string, error) {
 		area = user.BindArea.String
 		return nil
 	}); err != nil {
+		if errors.Is(err, models.ErrAlreadySignIn) {
+			return "今日已签到", nil
+		}
 		return "", err
 	}
 	if area == "" {
 		return "[未绑定位置]\n" +
 			"例如: 绑定位置 深圳", nil
 	}
+	// TODO 缓存天气
 	data, err := weComCn.Get(area)
 	if err != nil {
 		return "", err
 	}
-	return "[今日天气]\n" +
+	return "签到成功！\n" +
+		"[今日天气]\n" +
 		data.Time.Format("2006-01-02") + "\n" +
 		data.City + "\n" +
 		data.Weather + "\n" +
---
+++ internal/models/qsignin.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"context"
+	"database/sql"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"time"
+)
+
+type QSignIn struct {
+	ID       int64
+	QUID     int
+	Day      string
+	CreateAt time.Time
+}
+
+func (s QSignIn) TableName() string {
+	return "q_sign_in"
+}
+
+var ErrAlreadySignIn = errors.New("already sign in today")
+
+func (s *QSignIn) Insert(ctx context.Context, tx *sqlx.Tx) error {
+	if err := s.GetQSignInByQUID(ctx, tx); err != nil {
+		return err
+	}
+	query := `insert into q_sign_in (quid,create_at,day) values ($1,$2,$3) returning id`
+	if err := tx.GetContext(ctx, &s.ID, query, s.QUID, time.Now(), time.Now().Format("2006-01-02")); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (s *QSignIn) GetQSignInByQUID(ctx context.Context, tx *sqlx.Tx) error {
+	query := `select * from q_sign_in where quid = $1 and create_at > $2`
+	err := tx.GetContext(ctx, s, query, s.QUID, time.Now())
+	if err == nil {
+		return errors.WithStack(ErrAlreadySignIn)
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return errors.WithStack(err)
+	}
+	return nil
+}
